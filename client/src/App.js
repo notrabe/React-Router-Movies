09a9af45ec9928dff916c851afb4306f22456e0c
Evan Barton <notrabe1011@gmail.com>
2020-08-17 18:59:57 -0400

    completed all mvp tasks except one

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link, Route, Switch} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -21,14 +24,28 @@ const App = () => {
     getMovies();
   }, []);
 
+  console.log(movieList)
+
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
+
+
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route path='/'>
+              <MovieList movies = {movieList}/>
+          </Route>
+
+          <Route path='/movies/:id'>
+            <Movie movies = {movieList}/>  
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 };
